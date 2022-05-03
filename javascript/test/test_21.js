feeb7bdd48dcf378878bfc2ee34089f9947b7f35
kjun <kjun_all@naver.com>
2022-05-03 11:51:35 +0900

    coding test

--- javascript/test/test_21.js
+++ javascript/test/test_21.js
@@ -48,3 +48,32 @@ for (let i = 0; i < s.length; i++) {
 }
 
 return count === 0 ? true : false;
+
+// 프로그래머스 완주하지 못한 선수 풀이 정리
+/**
+ * 문제 설명
+
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+participant	completion	return
+["leo", "kiki", "eden"]	["eden", "kiki"]	"leo"
+["marina", "josipa", "nikola", "vinko", "filipa"]	["josipa", "filipa", "marina", "nikola"]	"vinko"
+["mislav", "stanko", "mislav", "ana"]	["stanko", "ana", "mislav"]	"mislav"
+ 
+처음 문제를 보고 fileter를 쓰려고 했다.
+
+filter를 사용하게 되면 첫번째, 두번째는 return 값이 잘 나오게 되지만
+
+ */
+
+participant.sort();
+completion.sort();
+
+for (let i = 0; i < participant.length; i++) {
+  if (participant[i] != completion[i]) {
+    answer = participant[i];
+    break;
+  }
+}
