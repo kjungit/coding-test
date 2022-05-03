28a835dddba331463c0b248ea06d829ffea17dba
kjun <kjun_all@naver.com>
2022-05-03 12:01:42 +0900

    coding test

--- javascript/test/test_21.js
+++ javascript/test/test_21.js
@@ -84,3 +84,23 @@ var solution = (_, $) =>
     (_) => !$[_]--,
     $.map((_) => ($[_] = ($[_] | 0) + 1))
   );
+
+// for, map, set을 사용한 풀이
+
+function solution(participant, completion) {
+  const map = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    let a = participant[i],
+      b = completion[i];
+
+    map.set(a, (map.get(a) || 0) + 1);
+    map.set(b, (map.get(b) || 0) - 1);
+  }
+
+  for (let [k, v] of map) {
+    if (v > 0) return k;
+  }
+
+  return "nothing";
+}
