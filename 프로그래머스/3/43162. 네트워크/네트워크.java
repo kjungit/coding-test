25b576f5bfb03e83f0d97ee066c35c1d681ae48e
kwonbeomjun <kjundwa@gmail.com>
2026-06-21 18:19:41 +0900

    [level 3] Title: 네트워크, Time: 1.68 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,6 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if(visited[i]) continue;
+            answer++;
+            visitAllConnectedComputers(computers, visited, i);
+        }
+
         return answer;
+    }
+
+
+    void visitAllConnectedComputers(final int[][] computers, boolean[] visited, int c) {
+        Stack<Integer> q = new Stack<>();
+        q.push(c);
+        while(!q.isEmpty()) {
+            int i = q.pop();
+            visited[i] = true;
+            for (int j = 0; j < computers[i].length; j++) {
+                if (visited[j]) continue;
+                if (computers[i][j] == 1) q.push(j);
+            }
+        }
     }
 }
