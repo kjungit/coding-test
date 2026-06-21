8c337c73c8ce4ed1deb6a62d0836f78d2c72c6ef
beomjunkwon <100064540+kjungit@users.noreply.github.com>
2026-06-21 19:52:35 +0900

    [level 2] Title: 타겟 넘버, Time: 3.10 ms, Memory: 81 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] numbers, int target) {
+        return sumCount(numbers, target, 0, 0);
+    }
+    
+    int sumCount(final int[] numbers, final int target, int i, int sum) {
+        if (i == numbers.length) {
+            if (sum == target) return 1;
+            return 0;
+        }
+        
+        return sumCount(numbers, target, i + 1, sum + numbers[i])
+         + sumCount(numbers, target, i + 1, sum - numbers[i]);
+    }
+}
